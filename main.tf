2f5362ed0b76caab92a2840a7526142406b42c60
Mckaiju <116754284+Mckaiju@users.noreply.github.com>
2022-11-01 13:22:04 -0400

    add hello world

--- main.tf
+++ main.tf
@@ -46,6 +46,8 @@ resource "aws_instance" "Demo" {
 
   curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
+  docker pull hello-world
+  dock run hello-world
   EOL
 
   tags = {
