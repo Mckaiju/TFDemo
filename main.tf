88a9d6cbae87fa689cce9249b96cf5e10a8fec15
Mckaiju <mckaiju@gmail.com>
2022-11-01 10:08:47 -0400

    install docker

--- main.tf
+++ main.tf
@@ -10,14 +10,32 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-017fecd1353bcc96e"
   instance_type = "t2.micro"
+  
+  user_data = <<-EOL
+  #!/bin/bash
+
+  apt-get update
+  apt-get install -y cloud-utils apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+  apt-get update
+  apt-get install -y docker-ce
+  usermod -aG docker ubuntu
+
+  curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+  EOL
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "TFDemo"
   }
 }
