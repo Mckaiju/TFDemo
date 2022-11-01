46a27bd852d85f4c60b505552d657d825b27f832
Mckaiju <116754284+Mckaiju@users.noreply.github.com>
2022-11-01 13:29:43 -0400

    add ER

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "Demo" {
   curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
   docker pull hello-world
-  dock run hello-world
+  docker run hello-world
   EOL
 
   tags = {
