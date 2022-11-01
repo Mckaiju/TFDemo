97579da16c142782bbf9b58df197ec091420f07f
Mckaiju <mckaiju@gmail.com>
2022-11-01 11:17:49 -0400

    add user

--- main.tf
+++ main.tf
@@ -30,6 +30,8 @@ resource "aws_instance" "Demo" {
 
   curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
+
+  apt-get install ec2-instance-connect
   EOL
 
   tags = {
