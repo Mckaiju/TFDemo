b0168b6ed2cfcbc75521309b89949159093fb3c2
Mckaiju <mckaiju@gmail.com>
2022-11-01 11:36:09 -0400

    add ssh

--- main.tf
+++ main.tf
@@ -13,6 +13,23 @@ provider "aws" {
 resource "aws_instance" "Demo" {
   ami           = var.ami
   instance_type = var.instance_type
+  key_name= "aws_key"
+  vpc_security_group_ids = [aws_security_group.main.id]
+  
+  provisioner "remote-exec" {
+    inline = [
+      "touch hello.txt",
+      "echo helloworld remote provisioner >> hello.txt",
+    ]
+  }
+  connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file("/home/jmauney/aws_key")
+      timeout     = "4m"
+   }
+}
   
   user_data = <<-EOL
   #!/bin/bash
@@ -30,11 +47,45 @@ resource "aws_instance" "Demo" {
 
   curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
-
-  apt-get install ec2-instance-connect
   EOL
 
   tags = {
     Name = "TFDemo"
   }
 }
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
+}
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDbvRN/gvQBhFe+dE8p3Q865T/xTKgjqTjj56p1IIKbq8SDyOybE8ia0rMPcBLAKds+wjePIYpTtRxT9UsUbZJTgF+SGSG2dC6+ohCQpi6F3xM7ryL9fy3BNCT5aPrwbR862jcOIfv7R1xVfH8OS0WZa8DpVy5kTeutsuH5suehdngba4KhYLTzIdhM7UKJvNoUMRBaxAqIAThqH9Vt/iR1WpXgazoPw6dyPssa7ye6tUPRipmPTZukfpxcPlsqytXWlXm7R89xAY9OXkdPPVsrQdkdfhnY8aFb9XaZP8cm7EOVRdxMsA1DyWMVZOTjhBwCHfEIGoePAS3jFMqQjGWQd rahul@rahul-HP-ZBook-15-G2"
+}
+
--- variables.tf
+++ variables.tf
@@ -12,3 +12,7 @@ variable "instance_type" {
     type = string
     default = "t2.micro"
 }
+
+variable "instance_username" {
+  default = "ubuntu" 
+}
