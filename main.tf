9b48fa72bdfd9ca43ac7d4f613420712a95ae362
Mckaiju <mckaiju@gmail.com>
2022-11-01 10:38:12 -0400

    add vars

--- main.tf
+++ main.tf
@@ -2,20 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
     }
   }
-
-  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_instance" "Demo" {
+  ami           = var.ami
+  instance_type = var.instance_type
   
   user_data = <<-EOL
   #!/bin/bash
